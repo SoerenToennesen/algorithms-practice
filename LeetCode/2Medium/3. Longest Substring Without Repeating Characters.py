df3e02e54e443c4e059c3bd334f2162301a2213d
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-02-09 21:09:20 +0100

    3 - medium, fix

--- LeetCode/2Medium/3. Longest Substring Without Repeating Characters.py
+++ LeetCode/2Medium/3. Longest Substring Without Repeating Characters.py
@@ -2,19 +2,24 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         longest_substring = 0
-        temp_count = 0
-        seen_chars = []
-        for c in s:
-            if c in seen_chars:
-                if temp_count > longest_substring:
-                    longest_substring = temp_count
-                seen_chars = [c]
-                temp_count = 1
-            else:
-                temp_count += 1
-                seen_chars.append(c)
-        return longest_substring if longest_substring >= temp_count else temp_count
+        s_temp = s
+        while len(s_temp) > 0:
+            temp_count = 0
+            seen_chars = []
+            for c in s_temp:
+                if c in seen_chars:
+                    if temp_count > longest_substring:
+                        longest_substring = temp_count
+                    seen_chars = [c]
+                    temp_count = 1
+                else:
+                    temp_count += 1
+                    seen_chars.append(c)
+            if temp_count > longest_substring:
+                longest_substring = temp_count
+            s_temp = s_temp[1:]
+        return longest_substring
 
 #tests
 solution = Solution()
-print(solution.lengthOfLongestSubstring("abcabcbb"))
+print(solution.lengthOfLongestSubstring("asjrgapa"))
