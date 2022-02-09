f911212d851bb7b322e196a255e8dce9386d741f
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-02-09 19:42:33 +0100

    5 - Medium, Practice - fix

--- LeetCode/2Medium/5. Longest Palindromic Substring.py
+++ LeetCode/2Medium/5. Longest Palindromic Substring.py
@@ -2,8 +2,8 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         if s == "":
-            return 0
-        longest_sequence = 0
+            return ""
+        longest_sequence = ""
         current_sequence = ""
         for c in s:
             current_sequence += c
@@ -13,10 +13,10 @@ class Solution:
                 built_string1 += c_temp1
             for c_temp2 in reversed(current_sequence):
                 built_string2 += c_temp2
-            if built_string1 == built_string2 and len(current_sequence) > longest_sequence:
-                longest_sequence = len(current_sequence)
+            if built_string1 == built_string2 and len(current_sequence) > len(longest_sequence):
+                longest_sequence = current_sequence
         check_recursive = self.longestPalindrome(s[1:])
-        if check_recursive > longest_sequence:
+        if len(check_recursive) > len(longest_sequence):
             longest_sequence = check_recursive
         return longest_sequence
 
