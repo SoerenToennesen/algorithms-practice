8046c48d175d7edabf82fa6606ed1915c7b8f8b3
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-02-20 12:23:59 +0100

    978 - medium (fixed, mostly)

--- LeetCode/2Medium/978. Longest Turbulent Subarray.py
+++ LeetCode/2Medium/978. Longest Turbulent Subarray.py
@@ -1,59 +1,31 @@
 
 class Solution:
     def maxTurbulenceSize(self, arr: list[int]) -> int:
-        if len(arr) == 1: return 1
-        max_size = 0
-        temp_size = 0
-        start = False
-        direction = False #False mean we are increasing, true otherwise
-        for i in range(len(arr) - 1):
-
-            #print(arr[i], arr[i+1])
-
-            if arr[i] == arr[i+1]:
-                #print('test1', temp_size)
-                if temp_size >= max_size:
-                    max_size = temp_size + 1
-                temp_size = 1
-                start = False
-                continue
-
-            if not start or (arr[i] < arr[i+1] and not direction):
-                #print('test2', temp_size)
-                start = True
-                direction = True if arr[i] < arr[i+1] else False
-                temp_size += 1
-                continue
-
-            if not start or (arr[i] > arr[i+1] and direction):
-                #print('test3', temp_size)
-                start = True
-                direction = False if arr[i] > arr[i+1] else True
-                temp_size += 1
-                continue
-
-            #print('test4', temp_size)
-
-            if temp_size >= max_size:
-                max_size = temp_size
-
-            temp_size = 1
-            start = False
-            
-        
-
-        if temp_size > max_size:
-            print('what')
-            max_size = temp_size
-
-        return max_size
+        n = len(arr)
+        dp = []
+        for i in range(n):
+            dp.append(0)
+        for i in range(1, n - 1):
+            if arr[i] < arr[i-1] and arr[i] < arr[i+1]:
+                dp[i] = dp[i - 1] + 1
+            elif arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+                dp[i] = dp[i - 1] + 1
+            elif arr[i]==arr[i+1] and arr[i]==arr[i-1]:
+                dp[i] = 0
+            else:
+                dp[i] = 1
+        return max(dp) + 1
 
 #test
 solution = Solution()
-#print(solution.maxTurbulenceSize([9,4,2,10,7,8,8,1,9]))
-#print(solution.maxTurbulenceSize([4,8,12,16]))
-#print(solution.maxTurbulenceSize([100]))
-#print(solution.maxTurbulenceSize([100, 100, 101, 101, 102, 101]))
-print(solution.maxTurbulenceSize([0,1,1,0,1,0,1,1,0,0]))
-print(solution.maxTurbulenceSize([9,4,2,10,7,8,8,1,9]))
-print(solution.maxTurbulenceSize([3,2,4,4]))
+print(solution.maxTurbulenceSize([9,4,2,10,7,8,8,1,9])) #5 - fix
+print(solution.maxTurbulenceSize([4,8,12,16])) #2 - fix
+print(solution.maxTurbulenceSize([100])) #1
+print(solution.maxTurbulenceSize([100, 100, 101, 101, 102, 101])) #3
+print(solution.maxTurbulenceSize([0,1,1,0,1,0,1,1,0,0])) #5
+print(solution.maxTurbulenceSize([3,2,4,4])) #3
+print(solution.maxTurbulenceSize([3,2])) #2
+print(solution.maxTurbulenceSize([3,3])) #1
+print(solution.maxTurbulenceSize([3,2,3,2])) #4
+print(solution.maxTurbulenceSize([2,0,2,4,2,5,0,1,2,3])) #6
+print(solution.maxTurbulenceSize([0,0,1,0,1,0,1,1])) #6
