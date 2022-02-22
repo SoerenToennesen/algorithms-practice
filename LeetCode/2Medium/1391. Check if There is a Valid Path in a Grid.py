fade68af5a710fa09329c10f7ca2c079801458a9
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-02-22 21:40:52 +0100

    1391 - medium (added a better implementation commented, even though I think it is more incorrect

--- LeetCode/2Medium/1391. Check if There is a Valid Path in a Grid.py
+++ LeetCode/2Medium/1391. Check if There is a Valid Path in a Grid.py
@@ -50,3 +50,49 @@ print(solution.hasValidPath(grid = [[1,2,1],[1,2,1]]))
 print(solution.hasValidPath(grid = [[1,1,2]]))
 print(solution.hasValidPath(grid = [[1,1,1,1,1,1,3]])) #this doesnt work, because it checks incorrectly in the direction_tiles, I think
 
+#for some reason, the below code works better, even though I feel there is some problems with having to know which direction you are going.
+'''
+class Solution:
+    def hasValidPath(self, grid: list[list[int]]) -> bool:
+        if len(grid) == 1 and len(grid[0]) == 1:
+            return True
+        directions = [[0,-1], [1,0], [0,1], [-1,0]] #down, right, up, left
+        directions_tiles = {
+            1: [1,4,6,1,3,5], #left, right
+            2: [2,3,4,2,5,6], #up, down
+            3: [1,4,6,2,5,6], #left, down
+            4: [1,3,5,2,5,6], #right, down
+            5: [2,3,4,1,4,6], #up, left
+            6: [2,3,4,1,3,5]  #up, right
+        }
+        n = len(grid)
+        m = len(grid[0])
+        grid_visited = []
+        for i in range(n):
+            grid_visited.append([])
+            for j in range(m):
+                grid_visited[i].append([grid[i][j], False])
+        
+        current = [0,0]
+        res = False
+        while True:
+            grid_visited[current[0]][current[1]][1] = True
+            tile_type = grid_visited[current[0]][current[1]][0]
+            should_continue = False
+            for direct in directions:
+                test_tile = [current[0] + direct[0], current[1] + direct[1]]
+                if not (test_tile[0] >= 0 and test_tile[0] < n and test_tile[1] >= 0 and test_tile[1] < m):
+                    continue
+                tile_test_type = grid_visited[test_tile[0]][test_tile[1]][0]
+                if (not grid_visited[test_tile[0]][test_tile[1]][1]) and tile_test_type in directions_tiles[tile_type]:
+                    if test_tile[0] == n - 1 and test_tile[1] == m - 1:
+                        res = True
+                        break
+                    current = test_tile
+                    should_continue = True
+                    break
+            if should_continue: continue
+            break
+
+        return res
+'''
