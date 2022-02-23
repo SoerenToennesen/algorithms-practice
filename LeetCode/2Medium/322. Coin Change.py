d4e96ad250abfffaac933811f6d6d60609d479d9
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-02-23 10:34:18 +0100

    322 - medium (practice, dynamic programming)

---
+++ LeetCode/2Medium/322. Coin Change.py
@@ -0,0 +1,25 @@
+
+#dynamic programming problem
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        if amount == 0: return 0
+        
+        dp = []
+        for _ in range(amount + 1):
+            dp.append(float('inf'))
+
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for j in range(0, len(coins)):
+                if coins[j] <= i:
+                    rest = dp[i - coins[j]]
+                    if rest != float('inf') and rest + 1 < dp[i]:
+                        dp[i] = rest + 1
+
+        return -1 if dp[amount] == float('inf') else dp[amount]
+
+#test
+solution = Solution()
+print(solution.coinChange(coins = [1,2,5], amount = 11)) #3
+print(solution.coinChange(coins = [2], amount = 3)) #-1
+print(solution.coinChange(coins = [1], amount = 0)) #0
