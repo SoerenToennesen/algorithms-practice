b2f09715e4b436b83eb3ea8c24c39a998055725e
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-02-28 18:25:48 +0100

    minimum coin change (practice - dynamic programming)

--- Others/General Questions/Minimum Coin Change.py
+++ Others/General Questions/Minimum Coin Change.py
@@ -1,10 +1,21 @@
-
+import sys
 class Solution:
-    def minimumCoinChange(arr: list[int], target: int) -> int:
-        arr.sort()
-        arr = set(arr)
-        print(arr[0])
-        return -1
+    def minimumCoinChange(self, arr: list[int], target: int) -> int:
+        #make sure input is correct for our algorithm
+
+        #dynamic programming algorithm
+        dp = [0 for i in range(target + 1)]
+        dp[0] = 0
+        for i in range(1, target + 1):
+            dp[i] = sys.maxsize
+        for i in range(1, target + 1):
+            for j in range(len(arr)):
+                if arr[j] <= i:
+                    temp = dp[i - arr[j]]
+                    if temp != sys.maxsize and temp + 1 < dp[i]:
+                        dp[i] = temp + 1
+        if dp[target] == sys.maxsize: return -1
+        return dp[target]
 
 #test
 solution = Solution()
