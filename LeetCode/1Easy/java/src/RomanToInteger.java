02c6e0f413afe24bfdbc5dfde93dc921319d7296
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-03-31 00:18:47 +0200

    13 - easy (practice)

--- LeetCode/1Easy/java/src/RomanToInteger.java
+++ LeetCode/1Easy/java/src/RomanToInteger.java
@@ -1,12 +1,44 @@
 
 // 13. Roman to Integer
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInteger {
 
-
+    private static final Map<Character, Integer> romanToIntegerMap = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000
+    );
 
     private int solution(String s) {
         int num = 0;
+        int checkNext = 0;
+        for (char c : s.toCharArray()) {
+            if (checkNext != 0) {
+                if (romanToIntegerMap.get(c) != checkNext) {
+                    num += romanToIntegerMap.get(c) - checkNext;
+                } else {
+                    num += romanToIntegerMap.get(c) + checkNext;
+                }
+                checkNext = 0;
+            } else {
+                if (romanToIntegerMap.get(c) == 1 ||
+                    romanToIntegerMap.get(c) == 10 ||
+                    romanToIntegerMap.get(c) == 100
+                ) {
+                    checkNext = romanToIntegerMap.get(c);
+                } else {
+                    num += romanToIntegerMap.get(c);
+                }
+            }
+        }
+        if (checkNext != 0) num += checkNext;
         return num;
     }
 
