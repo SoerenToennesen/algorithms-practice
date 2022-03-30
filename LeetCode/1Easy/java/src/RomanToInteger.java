7d3df1036d2bd347a05029e21b2fc72b9b74289b
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-03-30 23:06:14 +0200

    13 - easy (setup)

---
+++ LeetCode/1Easy/java/src/RomanToInteger.java
@@ -0,0 +1,23 @@
+
+// 13. Roman to Integer
+
+public class RomanToInteger {
+
+
+
+    private int solution(String s) {
+        int num = 0;
+        return num;
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger romanToInteger = new RomanToInteger();
+        String test1 = "III";
+        String test2 = "LVIII";
+        String test3 = "MCMXCIV";
+        System.out.println(romanToInteger.solution(test1));
+        System.out.println(romanToInteger.solution(test2));
+        System.out.println(romanToInteger.solution(test3));
+    }
+
+}
