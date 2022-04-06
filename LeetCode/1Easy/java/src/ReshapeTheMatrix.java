ebd821548cfc519af506cb0a15e5e548dd04b6e5
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-04-06 20:51:41 +0200

    566 - easy (practice - inital thought)

--- LeetCode/1Easy/java/src/ReshapeTheMatrix.java
+++ LeetCode/1Easy/java/src/ReshapeTheMatrix.java
@@ -6,8 +6,20 @@ import java.util.Map;
 public class ReshapeTheMatrix {
 
     private int[][] solution(int[][] mat, int r, int c) {
-
-        return mat;
+        if (r * c != mat[0].length * mat.length) return mat;
+        int ret[][] = new int[r][c];
+        int l = 0; int k = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; i++) {
+                ret[l][k] = mat[i][j];
+                k++;
+                if (k > c - 1) {
+                    k = 0;
+                    l++;
+                }
+            }
+        }
+        return ret;
     }
 
     public static void main(String[] args) {
