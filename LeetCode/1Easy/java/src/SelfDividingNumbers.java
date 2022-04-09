8590831280175076d4338ea38cbf1c2433a6fabc
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-04-09 10:51:35 +0200

    728 - easy (setup)

---
+++ LeetCode/1Easy/java/src/SelfDividingNumbers.java
@@ -0,0 +1,15 @@
+
+// 728. Self Dividing Numbers
+
+class SelfDividingNumbers {
+    public int[] solution(int left, int right) {
+
+        return new int[] {left, right};
+    }
+    public static void main(String[] args) {
+        SelfDividingNumbers selfDividingNumbers = new SelfDividingNumbers();
+        System.out.println(selfDividingNumbers.solution(1, 22));
+        System.out.println(selfDividingNumbers.solution(47, 85));
+    }
+}
+
