5ed745eae12787d328e31c9bb3c85d577ab3f113
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-04-12 23:09:05 +0200

    1518 - easy (setup)

---
+++ LeetCode/1Easy/java/src/WaterBottles.java
@@ -0,0 +1,15 @@
+
+// 1518. Water Bottles
+
+class WaterBottles {
+    public int solution(int numBottles, int numExchange) {
+        int res = 0;
+        return numBottles;
+    }
+
+    public static void main(String[] args) {
+        WaterBottles waterBottles = new WaterBottles();
+        System.out.println(waterBottles.solution(9, 3));
+    }
+}
+
