39616db10ecb4a9e544c4e3a41cfa11f8e90067a
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-04-22 22:39:00 +0200

    860 - easy (setup)

---
+++ LeetCode/1Easy/java/src/LemonadeChange.java
@@ -0,0 +1,16 @@
+
+// 860. Lemonade Change
+
+public class LemonadeChange {
+
+    private boolean solution(int[] bills) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LemonadeChange lemonadeChange = new LemonadeChange();
+        System.out.println(lemonadeChange.solution(new int[] {5,5,5,10,20})); //true
+        System.out.println(lemonadeChange.solution(new int[] {5,5,10,10,20})); //true
+    }
+
+}
