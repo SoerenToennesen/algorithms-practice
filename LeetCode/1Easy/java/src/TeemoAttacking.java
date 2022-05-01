a7f2b54b0d8525c69e5dbc47b43ba11c9ba91ff7
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-01 23:36:23 +0200

    495 - easy (practice)

---
+++ LeetCode/1Easy/java/src/TeemoAttacking.java
@@ -0,0 +1,16 @@
+
+// 495. Teemo Attacking
+
+class TeemoAttacking {
+    public int[][] solution(int[] timeSeries, int duration) {
+        int[][] test = new int[][] {{1,2,3},{1,2,3}};
+        return test;
+    }
+
+    public static void main(String[] args) {
+        TeemoAttacking teemoAttacking = new TeemoAttacking();
+        System.out.println(teemoAttacking.solution(new int[] {1,4}, 2)); //4
+        System.out.println(teemoAttacking.solution(new int[] {1,2}, 2)); //3
+    }
+}
+
