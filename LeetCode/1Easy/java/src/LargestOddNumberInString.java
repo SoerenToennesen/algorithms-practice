0852ef19db4851b0a6d98a23bbfcd10b4412589a
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-05 22:32:44 +0200

    1903 - easy (initialization)

---
+++ LeetCode/1Easy/java/src/LargestOddNumberInString.java
@@ -0,0 +1,16 @@
+
+// 1903. Largest Odd Number in String
+
+class LargestOddNumberInString {
+    public String solution(String num) {
+
+        return num;
+    }
+
+    public static void main(String[] args) {
+        LargestOddNumberInString largestOddNumberInString = new LargestOddNumberInString();
+        System.out.println(largestOddNumberInString.solution("52")); //"5"
+        System.out.println(largestOddNumberInString.solution("4206")); //""
+    }
+}
+
