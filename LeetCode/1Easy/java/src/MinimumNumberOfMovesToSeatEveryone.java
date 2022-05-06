2049cac313f0f69993f4be89539b42d32d07bff5
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-06 22:36:30 +0200

    2037 - easy (initial)

---
+++ LeetCode/1Easy/java/src/MinimumNumberOfMovesToSeatEveryone.java
@@ -0,0 +1,15 @@
+
+// 2037. Minimum Number of Moves to Seat Everyone
+
+class MinimumNumberOfMovesToSeatEveryone {
+    public int solution(int[] seats, int[] students) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        MinimumNumberOfMovesToSeatEveryone minimumNumberOfMovesToSeatEveryone = new MinimumNumberOfMovesToSeatEveryone();
+        System.out.println(minimumNumberOfMovesToSeatEveryone.solution(new int[] {3,1,5}, new int[] {2,7,4})); //4
+        System.out.println(minimumNumberOfMovesToSeatEveryone.solution(new int[] {4,1,5,9}, new int[] {1,3,2,6})); //7
+    }
+}
+
