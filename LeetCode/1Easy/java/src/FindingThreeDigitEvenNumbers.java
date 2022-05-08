7b609715e25905ab9b3ac3bb82ddda4388f960a1
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-08 19:58:40 +0200

    2094 - easy (practice, cheat)

--- LeetCode/1Easy/java/src/FindingThreeDigitEvenNumbers.java
+++ LeetCode/1Easy/java/src/FindingThreeDigitEvenNumbers.java
@@ -1,10 +1,24 @@
 
+// Solution gotten from https://leetcode.com/problems/finding-3-digit-even-numbers/discuss/1998041/Java-simple
 // 2094. Finding 3-Digit Even Numbers
+
+import java.util.HashSet;
+import java.util.Set;
 
 class FindingThreeDigitEvenNumbers {
     public int[] solution(int[] digits) {
-
-        return digits;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == 0) continue;
+            for (int j = 0; j < digits.length; j++) {
+                if (j == i) continue;
+                for (int k = 0; k < digits.length; k++) {
+                    if (k == i || k == j) continue;
+                    if (digits[k] % 2 == 0) set.add(digits[i] * 100 + digits[j] * 10 + digits[k]);
+                }
+            }
+        }
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(String[] args) {
