435ca3a5a5c091b7f1e6feac92486faed3d4bec1
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-14 23:15:48 +0200

    171 - easy (setup)

---
+++ LeetCode/1Easy/java/src/ExcelSheetColumnNumber.java
@@ -0,0 +1,17 @@
+
+// 171. Excel Sheet Column Number
+
+class ExcelSheetColumnNumber {
+    public int solution(String columnTitle) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ExcelSheetColumnNumber excelSheetColumnNumber = new ExcelSheetColumnNumber();
+        System.out.println(excelSheetColumnNumber.solution("A")); //1
+        System.out.println(excelSheetColumnNumber.solution("AB")); //28
+        System.out.println(excelSheetColumnNumber.solution("ZY")); //701
+    }
+}
+
