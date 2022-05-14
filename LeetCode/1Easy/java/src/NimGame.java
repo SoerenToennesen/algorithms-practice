242eea5525aa5f50f2d76a8eb916219550155962
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-15 00:22:45 +0200

    292 - easy (setup)

---
+++ LeetCode/1Easy/java/src/NimGame.java
@@ -0,0 +1,16 @@
+
+// 292. Nim Game
+
+class NimGame {
+    public boolean solution(int n) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        NimGame nimGame = new NimGame();
+        System.out.println(nimGame.solution(4); //false
+        System.out.println(nimGame.solution(1); //true
+        System.out.println(nimGame.solution(1); //true
+    }
+}
+
