6483fcde8a186a9820fd7076ff70f05296b9bdde
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-20 23:13:57 +0200

    1725 - easy (setup)

---
+++ LeetCode/1Easy/java/src/NumberOfRectanglesThatCanFormTheLargestSquare.java
@@ -0,0 +1,15 @@
+
+// 1725. Number Of Rectangles That Can Form The Largest Square
+
+class NumberOfRectanglesThatCanFormTheLargestSquare {
+    public int solution(int[][] rectangles) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        NumberOfRectanglesThatCanFormTheLargestSquare numberOfRectanglesThatCanFormTheLargestSquare = new NumberOfRectanglesThatCanFormTheLargestSquare();
+        System.out.println(numberOfRectanglesThatCanFormTheLargestSquare.solution(new int[][] {{5,8},{3,9},{5,12},{16,5}})); //3
+        System.out.println(numberOfRectanglesThatCanFormTheLargestSquare.solution(new int[][] {{2,3},{3,7},{4,3},{3,7}})); //3
+    }
+}
+
