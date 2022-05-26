2752f035fb4cdd275da1813cefdb87f60608c9c8
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-26 23:43:24 +0200

    896 - easy (setup)

---
+++ LeetCode/1Easy/java/src/MonotonicArray.java
@@ -0,0 +1,17 @@
+
+// 896. Monotonic Array
+
+class MonotonicArray {
+    public boolean solution(int[] nums) {
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        MonotonicArray monotonicArray = new MonotonicArray();
+        System.out.println(monotonicArray.solution(new int[] {1,2,2,3})); //true
+        System.out.println(monotonicArray.solution(new int[] {6,5,4,4})); //true
+        System.out.println(monotonicArray.solution(new int[] {1,3,2})); //false
+    }
+}
+
