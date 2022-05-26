950d7b30c6b3fb2e526c242d2f169095ffb192be
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-26 23:49:13 +0200

    896 - easy (practice)

--- LeetCode/1Easy/java/src/MonotonicArray.java
+++ LeetCode/1Easy/java/src/MonotonicArray.java
@@ -3,8 +3,16 @@
 
 class MonotonicArray {
     public boolean solution(int[] nums) {
-
-        return false;
+        if (nums.length < 2) return true;
+        boolean increasing = nums[0] <= nums[nums.length - 1];
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (increasing) {
+                if (nums[i] > nums[i+1]) return false;
+            } else {
+                if (nums[i] < nums[i+1]) return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
