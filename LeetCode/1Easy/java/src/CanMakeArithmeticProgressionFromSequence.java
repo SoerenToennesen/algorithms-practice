1e36eb76fab2bbe817b393ce1a3b049399959a99
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-26 23:57:26 +0200

    1502 - easy (practice)

--- LeetCode/1Easy/java/src/CanMakeArithmeticProgressionFromSequence.java
+++ LeetCode/1Easy/java/src/CanMakeArithmeticProgressionFromSequence.java
@@ -1,9 +1,17 @@
 
 // 1502. Can Make Arithmetic Progression From Sequence
 
+import java.util.Arrays;
+
 class CanMakeArithmeticProgressionFromSequence {
     public boolean solution(int[] arr) {
-        return false;
+        if (arr.length < 2) return true;
+        Arrays.sort(arr);
+        int difference = arr[1] - arr[0];
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i+1] - arr[i] != difference) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
