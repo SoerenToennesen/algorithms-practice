3e922bd46f7dbc6704d698a81bd09d3e8b812210
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-28 14:47:25 +0200

    876 - easy (practice)

--- LeetCode/1Easy/java/src/MiddleOfTheLinkedList.java
+++ LeetCode/1Easy/java/src/MiddleOfTheLinkedList.java
@@ -1,15 +1,21 @@
 
 // 876. Middle of the Linked List
 
+import java.util.Arrays;
+
 class MiddleOfTheLinkedList {
     public int[] solution(int[] head) {
-        return head;
+        return Arrays.copyOfRange(head, head[(int) Math.ceil(head.length / 2 - 1)], head.length);
     }
 
     public static void main(String[] args) {
         MiddleOfTheLinkedList middleOfTheLinkedList = new MiddleOfTheLinkedList();
         System.out.println(middleOfTheLinkedList.solution(new int[] {1,2,3,4,5})); //[3,4,5]
         System.out.println(middleOfTheLinkedList.solution(new int[] {1,2,3,4,5,6})); //[4,5,6]
+        int[] test = middleOfTheLinkedList.solution(new int[] {1,2,3,4,5,6});
+        for (int i = 0; i < test.length; i++) {
+            System.out.print(test[i]);
+        }
     }
 }
 
