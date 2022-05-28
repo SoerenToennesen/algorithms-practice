dbafbf4a10e4f1c94f6f2a72e539f4721b6f7684
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-28 14:34:14 +0200

    876 - easy (setup)

---
+++ LeetCode/1Easy/java/src/MiddleOfTheLinkedList.java
@@ -0,0 +1,15 @@
+
+// 876. Middle of the Linked List
+
+class MiddleOfTheLinkedList {
+    public int[] solution(int[] head) {
+        return head;
+    }
+
+    public static void main(String[] args) {
+        MiddleOfTheLinkedList middleOfTheLinkedList = new MiddleOfTheLinkedList();
+        System.out.println(middleOfTheLinkedList.solution(new int[] {1,2,3,4,5})); //[3,4,5]
+        System.out.println(middleOfTheLinkedList.solution(new int[] {1,2,3,4,5,6})); //[4,5,6]
+    }
+}
+
