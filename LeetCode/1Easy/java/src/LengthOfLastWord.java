81bf0ddea607c3f09156c716edc38a4625fe33b4
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-29 23:02:22 +0200

    58 - easy (setup)

---
+++ LeetCode/1Easy/java/src/LengthOfLastWord.java
@@ -0,0 +1,17 @@
+
+// 58. Length of Last Word
+
+class LengthOfLastWord {
+    public int solution(String s) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+        System.out.println(lengthOfLastWord.solution("Hello World")); //5
+        System.out.println(lengthOfLastWord.solution("   fly me   to   the moon  ")); //4
+        System.out.println(lengthOfLastWord.solution("luffy is still joyboy")); //6
+    }
+}
+
