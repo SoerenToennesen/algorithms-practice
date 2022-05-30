a5f7cb82a027d09d969eba1ef38c0b6311419921
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-30 22:51:45 +0200

    TimeConversion - HackerRank - bugfix

--- HackerRank/OneMonthPreparation/src/TimeConversion.java
+++ HackerRank/OneMonthPreparation/src/TimeConversion.java
@@ -1,16 +1,8 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
-class Result {
+import static java.util.stream.Collectors.joining;
+
+class ResultTimeConversion {
 
     /*
      * Complete the 'timeConversion' function below.
@@ -21,8 +13,9 @@ class Result {
 
     public static String timeConversion(String s) {
         // Write your code here
-        boolean isAM = s.charAt(s.length() - 2) == 'A';
-        int hour = Integer.parseInt(s.substring(0,2)) + (!isAM ? 12 : 0) % 24;
+        boolean addHours = (s.charAt(s.length() - 2) == 'A' && (Integer.parseInt(s.substring(0,2)) == 12)) ||
+                (s.charAt(s.length() - 2) != 'A' && !(Integer.parseInt(s.substring(0,2)) == 12));
+        int hour = (Integer.parseInt(s.substring(0,2)) + (addHours ? 12 : 0)) % 24;
         return (hour < 10 ? "0" + hour : hour) + s.substring(2,s.length() - 2);
     }
 
@@ -31,16 +24,17 @@ class Result {
 public class TimeConversion {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         String s = bufferedReader.readLine();
 
-        String result = Result.timeConversion(s);
+        String result = ResultTimeConversion.timeConversion(s);
+        System.out.println(result);
 
-        bufferedWriter.write(result);
-        bufferedWriter.newLine();
+        //bufferedWriter.write(result);
+        //bufferedWriter.newLine();
 
         bufferedReader.close();
-        bufferedWriter.close();
+        //bufferedWriter.close();
     }
 }
