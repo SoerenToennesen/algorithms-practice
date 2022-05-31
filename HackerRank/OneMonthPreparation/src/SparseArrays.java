db608d068e4a6c12b094a2c7211f21cc2b687d80
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-05-31 22:49:32 +0200

    SpareArrays - HackerRank

---
+++ HackerRank/OneMonthPreparation/src/SparseArrays.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultSpareArrays {
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        // Write your code here
+        ArrayList<Integer> result = new ArrayList<>();
+        for (String query: queries) {
+            int intermediateResult = 0;
+            for (String string : strings) {
+                if (query.contains(string)) intermediateResult++;
+            }
+            result.add(intermediateResult);
+        }
+        return result;
+    }
+
+}
+
+public class SparseArrays {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int stringsCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> strings = IntStream.range(0, stringsCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .collect(toList());
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .collect(toList());
+
+        List<Integer> res = ResultSpareArrays.matchingStrings(strings, queries);
+
+        bufferedWriter.write(
+                res.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
