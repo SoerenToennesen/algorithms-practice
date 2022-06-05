d7993152689f3ff01e1e34ed181f6f9ef4a954a8
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-05 23:21:13 +0200

    HackerRank - Drawing book - Setup

---
+++ HackerRank/OneMonthPreparation/src/DrawingBook.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
+
+class ResultDrawingBook {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p) {
+        // Write your code here
+
+        return n;
+    }
+
+}
+
+public class DrawingBook {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = ResultDrawingBook.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
