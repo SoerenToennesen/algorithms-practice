b88356a5e75cea35e078758695be5741d8889f37
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-05 11:24:53 +0200

    HackerRank - Two Arrays - First attempt

--- HackerRank/OneMonthPreparation/src/TwoArrays.java
+++ HackerRank/OneMonthPreparation/src/TwoArrays.java
@@ -18,8 +18,14 @@ class ResultTwoArrays {
 
     public static String twoArrays(int k, List<Integer> A, List<Integer> B) {
         // Write your code here
-
-        return "";
+        A.sort(Integer::compareTo);
+        B.sort(Comparator.reverseOrder());
+        for (int i = 0; i < A.size(); i++) {
+            if (A.get(i) + B.get(i) < k) {
+                return "NO";
+            }
+        }
+        return "YES";
     }
 
 }
