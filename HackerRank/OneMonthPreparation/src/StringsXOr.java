75c7b33a0b7c0b517e4235c6d6cda4ba550d7b8d
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-05 13:12:07 +0200

    ran some tests

---
+++ HackerRank/OneMonthPreparation/src/StringsXOr.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class StringsXOr {
+
+    public static void main(String[] args) throws IOException {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner reader = new Scanner(System.in);
+        String str1 = reader.next();
+        String str2 = reader.next();
+        reader.close();
+        int length = Integer.max(str1.length(), str2.length());
+        int str1Int = Integer.parseInt(str1);
+        int str2Int = Integer.parseInt(str2);
+        int xorResult = str1Int ^ str2Int;
+        System.out.println(String.valueOf(xorResult));
+        //String binaryFormat = "%" + String.format("%d", length) + "s";
+        //String result = String.format(binaryFormat, Integer.toBinaryString(xorResult)).replace(' ', '0');
+        //System.out.println(result);
+    }
+}
