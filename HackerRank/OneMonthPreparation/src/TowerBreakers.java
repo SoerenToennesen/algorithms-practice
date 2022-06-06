4ca08f138a1da602e6fc24001aa4c8a6f780d1b1
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-06 11:36:47 +0200

    HackerRank - Tower Breakers - Implemented

--- HackerRank/OneMonthPreparation/src/TowerBreakers.java
+++ HackerRank/OneMonthPreparation/src/TowerBreakers.java
@@ -15,7 +15,7 @@ class ResultTowerBreakers {
 
     public static int towerBreakers(int n, int m) {
         // Write your code here
-        return n;
+        return (m == 1 || n % 2 == 0) ? 2 : 1;
     }
 
 }
