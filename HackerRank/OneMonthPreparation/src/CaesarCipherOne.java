e328474c195d2bce8f9ecce3e34cf61af898f0ba
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-08 23:01:29 +0200

    HackerRank - Caesar Cipher 1 - Implemented

--- HackerRank/OneMonthPreparation/src/CaesarCipherOne.java
+++ HackerRank/OneMonthPreparation/src/CaesarCipherOne.java
@@ -15,7 +15,22 @@ class ResultCaesarCipherOne {
 
     public static String caesarCipher(String s, int k) {
         // Write your code here
-        return s;
+        String res = "";
+        for (char c : s.toCharArray()) {
+            int ascii = (int) c;
+            if (!((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122))) {
+                res += c;
+                continue;
+            }
+            if (Character.isLowerCase(c)) {
+                ascii -= 97;
+            } else {
+                ascii -= 65;
+            }
+            ascii = (ascii + k) % 26;
+            res += (char) (ascii + (Character.isLowerCase(c) ? 97 : 65));
+        }
+        return res;
     }
 
 }
@@ -23,7 +38,7 @@ class ResultCaesarCipherOne {
 public class CaesarCipherOne {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
@@ -32,11 +47,12 @@ public class CaesarCipherOne {
         int k = Integer.parseInt(bufferedReader.readLine().trim());
 
         String result = ResultCaesarCipherOne.caesarCipher(s, k);
+        System.out.println(result);
 
-        bufferedWriter.write(result);
-        bufferedWriter.newLine();
+        //bufferedWriter.write(result);
+        //bufferedWriter.newLine();
 
         bufferedReader.close();
-        bufferedWriter.close();
+        //bufferedWriter.close();
     }
 }
