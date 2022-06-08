56353112f0386b18650346855540bff6a1d82d17
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-08 19:47:40 +0200

    HackerRank - Caesar Cipher 1 - Setup

---
+++ HackerRank/OneMonthPreparation/src/CaesarCipherOne.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
+
+class ResultCaesarCipherOne {
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static String caesarCipher(String s, int k) {
+        // Write your code here
+        return s;
+    }
+
+}
+
+public class CaesarCipherOne {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = ResultCaesarCipherOne.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
