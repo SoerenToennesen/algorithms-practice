beb04ad5e72841f05d1515b2e86277ae2c1a35ec
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-11 16:53:50 +0200

    HackerRank - Prime Dates - Attempt 2

--- HackerRank/OneMonthPreparation/src/PrimeDates.java
+++ HackerRank/OneMonthPreparation/src/PrimeDates.java
@@ -29,9 +29,9 @@ public class PrimeDates {
 
     public static void updateLeapYear(int year) {
         if(year % 400 == 0) {
-            month[2] = 28;
-        } else if(year % 100 == 0) {
             month[2] = 29;
+        } else if(year % 100 == 0) {
+            month[2] = 28;
         } else if(year % 4 == 0) {
             month[2] = 29;
         } else {
