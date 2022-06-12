5c121fd0b4f6b28b6503d6229114d14173f231dc
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-12 14:02:05 +0200

    HackerRank - Recursive Digit Sum - Setup

---
+++ HackerRank/OneMonthPreparation/src/RecursiveDigitSum.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
+
+class ResultRecursiveDigitSum {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(String n, int k) {
+        // Write your code here
+        return k;
+    }
+
+}
+
+public class RecursiveDigitSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = ResultRecursiveDigitSum.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
