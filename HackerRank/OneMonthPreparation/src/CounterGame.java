1fc93619c05f34b9f2fde763eecf86af8f0c8a01
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-14 00:02:45 +0200

    HackerRank - Counter Game - Setup

---
+++ HackerRank/OneMonthPreparation/src/CounterGame.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+
+class ResultCounterGame {
+
+    /*
+     * Complete the 'counterGame' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static String counterGame(long n) {
+        // Write your code here
+        return "";
+    }
+
+}
+
+public class CounterGame {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                String result = ResultCounterGame.counterGame(n);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
