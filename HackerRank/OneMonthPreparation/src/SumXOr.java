499d1f9a499d62f77f2829f40919862573d784aa
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-14 00:22:22 +0200

    HackerRank - Sum XOr - Setup

---
+++ HackerRank/OneMonthPreparation/src/SumXOr.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
+
+class ResultSumXOr {
+
+    /*
+     * Complete the 'sumXor' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long sumXor(long n) {
+        // Write your code here
+        return -1;
+    }
+
+}
+
+public class SumXOr {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = ResultSumXOr.sumXor(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
