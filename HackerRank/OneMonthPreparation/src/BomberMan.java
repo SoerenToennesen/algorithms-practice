cbdba64e65074ba502a6dfcf68cdfe8399c53c83
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-15 23:25:55 +0200

    HackerRank - Bomber Man - Setup

---
+++ HackerRank/OneMonthPreparation/src/BomberMan.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultBomberMan {
+
+    /*
+     * Complete the 'bomberMan' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. STRING_ARRAY grid
+     */
+
+    public static List<String> bomberMan(int n, List<String> grid) {
+        // Write your code here
+
+    }
+
+}
+
+public class BomberMan {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int r = Integer.parseInt(firstMultipleInput[0]);
+
+        int c = Integer.parseInt(firstMultipleInput[1]);
+
+        int n = Integer.parseInt(firstMultipleInput[2]);
+
+        List<String> grid = IntStream.range(0, r).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .collect(toList());
+
+        List<String> result = ResultBomberMan.bomberMan(n, grid);
+
+        //bufferedWriter.write(
+        //        result.stream()
+        //                .collect(joining("\n"))
+        //                + "\n"
+        //);
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+    }
+}
