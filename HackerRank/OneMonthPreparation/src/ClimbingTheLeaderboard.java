20152675f91fc642f2d7fcfeb2e35879019fee18
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-20 23:13:31 +0200

    HackerRank - Climbing The Leaderboard - Attempt 1

--- HackerRank/OneMonthPreparation/src/ClimbingTheLeaderboard.java
+++ HackerRank/OneMonthPreparation/src/ClimbingTheLeaderboard.java
@@ -15,9 +15,35 @@ class ResultClimbingTheLeaderboard {
      *  2. INTEGER_ARRAY player
      */
 
+    private static int binarySearchForIndex(List<Integer> ranked, int low, int high, int target) {
+        int mid = -1;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (ranked.get(mid) > target) {
+                high = mid - 1;
+            } else if (ranked.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                while (mid > 0) {
+                    if (ranked.get(mid - 1) == target) mid--;
+                    else return mid;
+                }
+            }
+        }
+        return mid;
+    }
+
     public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
         // Write your code here
-        return ranked;
+        List<Integer> res = new ArrayList<>();
+        int scoreTemp = 0;
+        int low = 0; int high = ranked.size() - 1;
+        for (int i = 0; i < player.size(); i++) {
+            scoreTemp += player.get(i);
+            int index = binarySearchForIndex(ranked, low, high, scoreTemp);
+            res.add(index);
+        }
+        return res;
     }
 
 }
@@ -25,7 +51,7 @@ class ResultClimbingTheLeaderboard {
 public class ClimbingTheLeaderboard {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int rankedCount = Integer.parseInt(bufferedReader.readLine().trim());
 
@@ -41,14 +67,14 @@ public class ClimbingTheLeaderboard {
 
         List<Integer> result = ResultClimbingTheLeaderboard.climbingLeaderboard(ranked, player);
 
-        bufferedWriter.write(
+        /*bufferedWriter.write(
                 result.stream()
                         .map(Object::toString)
                         .collect(joining("\n"))
                         + "\n"
-        );
+        );*/
 
         bufferedReader.close();
-        bufferedWriter.close();
+        //bufferedWriter.close();
     }
 }
