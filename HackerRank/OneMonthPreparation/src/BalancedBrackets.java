ee9472a58c9820546d1f5abbd5865867b351c906
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-06-30 18:33:13 +0200

    HackerRank - Balanced Brackets - Setup

---
+++ HackerRank/OneMonthPreparation/src/BalancedBrackets.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+
+class ResultBalancedBrackets {
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String isBalanced(String s) {
+        // Write your code here
+        return s;
+    }
+
+}
+
+public class BalancedBrackets {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = ResultBalancedBrackets.isBalanced(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
