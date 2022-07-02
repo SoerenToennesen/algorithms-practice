52921286f79c6350fe727086b9444ad9efbd0412
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-07-02 22:57:38 +0200

    HackerRank - Waiter - Setup

---
+++ HackerRank/OneMonthPreparation/src/Waiter.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultWaiter {
+
+    /*
+     * Complete the 'waiter' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY number
+     *  2. INTEGER q
+     */
+
+    public static List<Integer> waiter(List<Integer> number, int q) {
+        // Write your code here
+        return number;
+    }
+
+}
+
+public class Waiter {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int q = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> number = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = ResultWaiter.waiter(number, q);
+
+//        bufferedWriter.write(
+//                result.stream()
+//                        .map(Object::toString)
+//                        .collect(joining("\n"))
+//                        + "\n"
+//        );
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+    }
+}
