8aab056ba5d05ebc848b7b6e2ad9815d2787f5e9
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-07-04 23:30:25 +0200

    HackerRank - Equal Stacks - Attempt 1

--- HackerRank/OneMonthPreparation/src/EqualStacks.java
+++ HackerRank/OneMonthPreparation/src/EqualStacks.java
@@ -18,6 +18,45 @@ class ResultEqualStacks {
 
     public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
         // Write your code here
+        Collections.reverse(h1);
+        Collections.reverse(h2);
+        Collections.reverse(h3);
+        int sum = 0;
+        for (int i = 0; i < h1.size(); i++) {
+            sum += h1.get(i);
+            h1.set(i, sum);
+        }
+        for (int i = 0; i < h2.size(); i++) {
+            sum += h2.get(i);
+            h2.set(i, sum);
+        }
+        for (int i = 0; i < h3.size(); i++) {
+            sum += h3.get(i);
+            h3.set(i, sum);
+        }
+        Collections.reverse(h1);
+        Collections.reverse(h2);
+        Collections.reverse(h3);
+        int smallestList = h1.size() < h2.size() ? h1.size() < h3.size() ? 1 : 3 : 2;
+        switch (smallestList) {
+            case 1:
+                for (int i = 0; i < h1.size(); i++) {
+                    if (h2.contains(h1.get(i)) && h3.contains(h1.get(i))) return h1.get(i);
+                }
+                break;
+            case 2:
+                for (int i = 0; i < h2.size(); i++) {
+                    if (h1.contains(h2.get(i)) && h3.contains(h2.get(i))) return h2.get(i);
+                }
+                break;
+            case 3:
+                for (int i = 0; i < h1.size(); i++) {
+                    if (h2.contains(h3.get(i)) && h1.contains(h3.get(i))) return h3.get(i);
+                }
+                break;
+            default:
+                break;
+        }
         return -1;
     }
 
