c6548b67015998c648aa45ce0784b3bda97d023d
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-07-15 12:29:17 +0200

    HackerRank - Jesse And Cookies - Setup

---
+++ HackerRank/OneMonthPreparation/src/JesseAndCookies.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultJesseAndCookies {
+
+    /*
+     * Complete the 'cookies' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY A
+     */
+
+    public static int cookies(int k, List<Integer> A) {
+        // Write your code here
+        return -1;
+    }
+
+}
+
+public class JesseAndCookies {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> A = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = ResultJesseAndCookies.cookies(k, A);
+
+        //bufferedWriter.write(String.valueOf(result));
+        //bufferedWriter.newLine();
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+    }
+}
