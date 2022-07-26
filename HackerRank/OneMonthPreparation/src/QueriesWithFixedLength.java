9389ee874b309d1cd3d0eb0f8c9b3bba1e521ccf
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-07-26 13:12:06 +0200

    HackerRank - Queries With Fixed Length - Setup

---
+++ HackerRank/OneMonthPreparation/src/QueriesWithFixedLength.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultQueriesWithFixedLength {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> solve(List<Integer> arr, List<Integer> queries) {
+        // Write your code here
+        return arr;
+    }
+
+}
+
+public class QueriesWithFixedLength {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int q = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> queries = IntStream.range(0, q).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = ResultQueriesWithFixedLength.solve(arr, queries);
+
+        /*bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );*/
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+    }
+}
