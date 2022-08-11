61801025d7c859ed993f032359aa04c7cb736465
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-08-11 23:11:33 +0200

    HackerRank - Lilys Homework - Setup

---
+++ HackerRank/OneMonthPreparation/src/LilysHomework.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultLilysHomework {
+
+    /*
+     * Complete the 'lilysHomework' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int lilysHomework(List<Integer> arr) {
+        // Write your code here
+        return -1;
+    }
+
+}
+
+public class LilysHomework {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = ResultLilysHomework.lilysHomework(arr);
+
+        //bufferedWriter.write(String.valueOf(result));
+        //bufferedWriter.newLine();
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+    }
+}
