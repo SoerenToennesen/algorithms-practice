e40704588c9de9a099a4954cefccbfc8ab6c0b02
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-09-05 22:05:06 +0200

    LeetCode - 787 - Setup

---
+++ LeetCode/2Medium/java/src/CheapestFlightsWithinKStops.java
@@ -0,0 +1,11 @@
+public class CheapestFlightsWithinKStops {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        CheapestFlightsWithinKStops solution = new CheapestFlightsWithinKStops();
+        solution.findCheapestPrice(4, new int[][] {{0,1,100}, {1,2,100}, {2,0,100}, {1,3,600}, {2,3,200}}, 0, 3, 1);
+    }
+}
