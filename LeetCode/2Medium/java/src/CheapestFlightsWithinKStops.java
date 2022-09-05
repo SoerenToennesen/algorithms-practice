d02bc91b13405880cd92deef538819a2553d6fe7
Søren Tønnesen - Personal <sorentoennesen@gmail.com>
2022-09-05 22:07:30 +0200

    LeetCode - 787 - Added TODOs

--- LeetCode/2Medium/java/src/CheapestFlightsWithinKStops.java
+++ LeetCode/2Medium/java/src/CheapestFlightsWithinKStops.java
@@ -1,11 +1,12 @@
 public class CheapestFlightsWithinKStops {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-
+        //TODO: Implement Dijkstras algorithm, based on some graph structure of the flights
         return -1;
     }
 
     public static void main(String[] args) {
         CheapestFlightsWithinKStops solution = new CheapestFlightsWithinKStops();
-        solution.findCheapestPrice(4, new int[][] {{0,1,100}, {1,2,100}, {2,0,100}, {1,3,600}, {2,3,200}}, 0, 3, 1);
+        int res = solution.findCheapestPrice(4, new int[][] {{0,1,100}, {1,2,100}, {2,0,100}, {1,3,600}, {2,3,200}}, 0, 3, 1);
+        System.out.println(res);
     }
 }
