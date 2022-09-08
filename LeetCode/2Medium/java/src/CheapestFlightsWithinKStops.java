d2da794947b7a82aaed67beb17b52aba7ee24ac7
s180381 <s180381@student.dtu.dk>
2022-09-08 23:47:56 +0200

    HackerRank - Cheapest Flights Within Stops - Attempt1

--- LeetCode/2Medium/java/src/CheapestFlightsWithinKStops.java
+++ LeetCode/2Medium/java/src/CheapestFlightsWithinKStops.java
@@ -1,6 +1,44 @@
+import java.util.*;
+
 public class CheapestFlightsWithinKStops {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         //TODO: Implement Dijkstras algorithm, based on some graph structure of the flights
+        Set<Integer> adj[] = new HashSet[n];
+        int[] dist = new int[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new HashSet<>();
+            dist[i] = -1;
+        }
+        for (int i = 0; i < n; i++) {
+            int v = flights[i][0];
+            int w = flights[i][1];
+            adj[v-1].add(w-1);
+            adj[w-1].add(v-1);
+        }
+        boolean visted[] = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        visted[src-1] = true;
+        queue.add(src-1);
+
+        while (queue.size() != 0) {
+            src = queue.poll();
+            if (dist[src] < 0) dist[src] = 0;
+            Iterator<Integer> iterator = adj[src].iterator();
+            while (iterator.hasNext()) {
+                int next = iterator.next();
+                if (!visted[next]) {
+                    visted[next] = true;
+                    queue.add(next);
+                    dist[next] = dist[src] + flights[src][2];
+                }
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (dist[i] != 0) {
+                res.add(dist[i]);
+            }
+        }
         return -1;
     }
 
