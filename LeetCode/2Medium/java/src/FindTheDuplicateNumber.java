26d33af6128e858cfca447a534ab16a49b182590
s180381 <s180381@student.dtu.dk>
2022-09-10 14:23:03 +0200

    LeetCode - 287 - Setup

---
+++ LeetCode/2Medium/java/src/FindTheDuplicateNumber.java
@@ -0,0 +1,13 @@
+class FindTheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        FindTheDuplicateNumber solution = new FindTheDuplicateNumber();
+        int res1 = solution.findDuplicate(new int[] {1,3,4,2,2});
+        int res2 = solution.findDuplicate(new int[] {3,1,3,4,2});
+        System.out.println("Duplciate numbers: " + res1 + ", " + res2);
+    }
+}
