6a50d58286b0063cf6ca70dcc8b025bc39a013b9
s180381 <s180381@student.dtu.dk>
2022-09-11 18:18:08 +0200

    LeetCode - 1010 - Setup

---
+++ LeetCode/2Medium/java/src/PairsOfSongsWithTotalDurationsDivisibleBySixty.java
@@ -0,0 +1,13 @@
+class PairsOfSongsWithTotalDurationsDivisibleBySixty {
+    public int numPairsDivisibleBy60(int[] time) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        PairsOfSongsWithTotalDurationsDivisibleBySixty solution = new PairsOfSongsWithTotalDurationsDivisibleBySixty();
+        int res1 = solution.numPairsDivisibleBy60(new int[] {30,20,150,100,40});
+        int res2 = solution.numPairsDivisibleBy60(new int[] {30,20,150,100,40});
+        System.out.println("Result: " + res1 + ", " res2);
+    }
+}
