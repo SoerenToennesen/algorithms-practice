2f27cfa00be6ef772d35b5a9febcdd1b15c423bc
s180381 <s180381@student.dtu.dk>
2022-09-12 16:36:30 +0200

    LeetCode - 1686 - Setup

---
+++ LeetCode/2Medium/java/src/StoneGameVI.java
@@ -0,0 +1,12 @@
+class StoneGameVI {
+    public int stoneGameVI(int[] aliceValues, int[] bobValues) {
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        StoneGameVI solution = new StoneGameVI();
+        int res1 = solution.stoneGameVI(new int[] {1,2}, new int[] {3,1});
+        int res2 = solution.stoneGameVI(new int[] {2,4,3}, new int[] {1,6,7});
+    }
+}
