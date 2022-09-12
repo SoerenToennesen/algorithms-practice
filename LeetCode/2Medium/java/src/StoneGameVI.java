1a44ae1b4705e645ef3652ca934a8e6ce584b7d0
s180381 <s180381@student.dtu.dk>
2022-09-12 16:47:03 +0200

    LeetCode - 1686 - Attempt 1

--- LeetCode/2Medium/java/src/StoneGameVI.java
+++ LeetCode/2Medium/java/src/StoneGameVI.java
@@ -1,7 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class ElementStoneGameVI implements Comparable<ElementStoneGameVI> {
+
+    int index, value;
+
+    ElementStoneGameVI(int index, int value){
+        this.index = index;
+        this.value = value;
+    }
+
+    public int compareTo(ElementStoneGameVI e) {
+        return this.value - e.value;
+    }
+}
+
 class StoneGameVI {
     public int stoneGameVI(int[] aliceValues, int[] bobValues) {
-
-        return -1;
+        List<ElementStoneGameVI> aliceElements = new ArrayList<>();
+        List<ElementStoneGameVI> bobElements = new ArrayList<>();
+        for (int i = 0; i < aliceValues.length; i++) {
+            aliceElements.add(new ElementStoneGameVI(i, aliceValues[i]));
+            bobElements.add(new ElementStoneGameVI(i, bobValues[i]));
+        }
+        Collections.reverse(aliceElements);
+        Collections.reverse(bobElements);
+        int alicePoints = 0;
+        int bobPoints = 0;
+        for (int i = 0; i < aliceValues.length / 2; i++) {
+            if (i % 2 == 0) {
+                alicePoints += aliceElements.get(i).value;
+            } else {
+                bobPoints += bobElements.get(i).value;
+            }
+            aliceElements.remove(i);
+            bobElements.remove(i);
+        }
+        return Integer.compare(alicePoints, bobPoints);
     }
 
     public static void main(String[] args) {
