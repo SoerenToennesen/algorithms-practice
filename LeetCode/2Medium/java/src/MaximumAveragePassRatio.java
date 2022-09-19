7f9da1362123ffb97eacc82b1570dce72aeb1311
s180381 <s180381@student.dtu.dk>
2022-09-19 17:59:52 +0200

    LeetCode - 1792 - Attempt 1

--- LeetCode/2Medium/java/src/MaximumAveragePassRatio.java
+++ LeetCode/2Medium/java/src/MaximumAveragePassRatio.java
@@ -1,7 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class MaximumAveragePassRatio {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-
-        return -1;
+        List<List<Float>> passPercentage = new ArrayList<>();
+        for (int i = 0; i < classes.length; i++) {
+            List<Float> tempList = new ArrayList<>();
+            tempList.add((float) classes[i][0] / classes[i][1]);
+            tempList.add((float) classes[i][0]);
+            tempList.add((float) classes[i][1]);
+            passPercentage.add(tempList);
+        }
+        // Also sort based on the lowest total, as adding students to a lower total will mathematically increase the pass rate of that class more than others.
+        Comparator<List<Float>> comparator1 = Comparator.comparing(floatList -> floatList.get(0));
+        Comparator<List<Float>> comparator2 = Comparator.comparing(floatList -> floatList.get(2));
+        Comparator<List<Float>> comparator = comparator1.thenComparing(comparator2);
+        Collections.sort(passPercentage, comparator);
+        // TODO: Make this loop more efficient
+        for (int i = 0; i < extraStudents; i++) {
+            List<Float> tempList = new ArrayList<>();
+            tempList.add((float) passPercentage.get(0).get(1) + 1 / passPercentage.get(0).get(2) + 1);
+            tempList.add(passPercentage.get(0).get(1) + 1);
+            tempList.add(passPercentage.get(0).get(2) + 1);
+            passPercentage.set(0, tempList);
+            Collections.sort(passPercentage, comparator);
+        }
+        double sum = 0;
+        for (int i = 0; i < passPercentage.size(); i++) {
+            sum += passPercentage.get(i).get(0);
+        }
+        sum /= passPercentage.size();
+        return sum;
     }
 
     public static void main(String[] args) {
