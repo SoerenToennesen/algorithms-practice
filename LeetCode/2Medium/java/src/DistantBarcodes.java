10e1601128abf91f8b46ea41afa3adad1f3b4ac2
= <sorentoennesen@gmail.com>
2022-09-27 19:52:52 +0200

    LeetCode - 1054 - Attempt 3

--- LeetCode/2Medium/java/src/DistantBarcodes.java
+++ LeetCode/2Medium/java/src/DistantBarcodes.java
@@ -18,6 +18,8 @@ public class DistantBarcodes {
                     }
                     j++;
                 }
+            } else {
+                temp = barcodes[i];
             }
         }
         return barcodes;
@@ -27,6 +29,16 @@ public class DistantBarcodes {
         DistantBarcodes solution = new DistantBarcodes();
         int[] res1 = solution.rearrangeBarcodes(new int[] {1,1,1,2,2,2}); //[2,1,2,1,2,1]
         int[] res2 = solution.rearrangeBarcodes(new int[] {1,1,1,1,2,2,3,3}); //[1,3,1,3,1,2,1,2]
-        System.out.println("Result: " + res1 + ", " + res2);
+        //System.out.println("Result: " + res1 + ", " + res2);
+        System.out.print("Result1: ");
+        for (int i = 0; i < res1.length; i++) {
+            System.out.print(res1[i] + ",");
+        }
+        System.out.println("");
+        System.out.print("Result2: ");
+        for (int i = 0; i < res2.length; i++) {
+            System.out.print(res2[i] + ",");
+        }
+        System.out.println("");
     }
 }
