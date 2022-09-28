8a8736023011c4b501433daea14abc0597e5059e
= <sorentoennesen@gmail.com>
2022-09-28 23:46:09 +0200

    LeetCode - 1540 - Attempt 1

--- LeetCode/2Medium/java/src/CanConvertStringInKMoves.java
+++ LeetCode/2Medium/java/src/CanConvertStringInKMoves.java
@@ -1,7 +1,18 @@
+import java.util.HashSet;
+
 public class CanConvertStringInKMoves {
     public boolean canConvertString(String s, String t, int k) {
-
-        return false;
+        if (s.length() != t.length()) return false;
+        HashSet<Integer> seen = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            int shift = Math.abs(s.charAt(i) - t.charAt(i));
+            if (seen.contains(shift) || shift > k) {
+                return false;
+            } else {
+                seen.add(shift);
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -9,6 +20,6 @@ public class CanConvertStringInKMoves {
         boolean res1 = solution.canConvertString("input", "output", 9); //true
         boolean res2 = solution.canConvertString("abc", "bcd", 10); //false
         boolean res3 = solution.canConvertString("aab", "bbb", 27); //true
-        System.out.println("Result: " + res1 + ", " + res2);
+        System.out.println("Result: " + res1 + ", " + res2 + ", " + res3);
     }
 }
