dd07fe84214da765133c986cad7e6da41ddc5d9e
= <sorentoennesen@gmail.com>
2022-10-14 19:06:50 +0200

    Leetcode - 553 - Implemented

--- LeetCode/2Medium/java/src/OptimalDivision.java
+++ LeetCode/2Medium/java/src/OptimalDivision.java
@@ -1,8 +1,17 @@
 public class OptimalDivision {
     public String optimalDivision(int[] nums) {
-        // TODO: Implement me
-
-        return "";
+        // Answer gotten from https://leetcode.com/problems/optimal-division/solution/
+        // 2 / 4 / 8 / 10
+        // 6 / 8 / 2 / 10 / 6 / 4
+        // Idea: you always want to respectively maximize and then minimize each next operation, starting with maximize
+        if (nums.length == 1) return nums[0] + "";
+        if (nums.length == 2) return nums[0] + "/" + nums[1];
+        StringBuilder res = new StringBuilder(nums[0] + "/(" + nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            res.append("/" + nums[i]);
+        }
+        res.append(")");
+        return res.toString();
     }
 
     public static void main(String[] args) {
