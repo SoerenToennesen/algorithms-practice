12d6d6d5ee61f790be398b75869036d37cdd4162
= <sorentoennesen@gmail.com>
2022-10-16 23:28:59 +0200

    Leetcode - 647 - Attempt 1

--- LeetCode/2Medium/java/src/PalindromicSubstrings.java
+++ LeetCode/2Medium/java/src/PalindromicSubstrings.java
@@ -1,8 +1,28 @@
 public class PalindromicSubstrings {
+    private boolean isPalindrome(String s) {
+        if (s.length() == 1) return true;
+        int p1 = 0;
+        int p2 = s.length() - 1;
+        while (p1 < p2) {
+            if (s.charAt(p1) != s.charAt(p2)) return false;
+        }
+        return true;
+    }
     public int countSubstrings(String s) {
-        // TODO: Implement me
-
-        return -1;
+        int skip = 0;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            skip = 0;
+            for (int j = s.length() - 1; j >= i; j--) {
+                if (isPalindrome(s.substring(i, j+1))) {
+                    skip = j - i;
+                    res += j - i;
+                    break;
+                }
+            }
+            i += skip;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
