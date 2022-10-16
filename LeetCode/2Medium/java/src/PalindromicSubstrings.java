cb177d3ee8217375bc2cf00fb5093fbdee9ec33c
= <sorentoennesen@gmail.com>
2022-10-16 23:13:09 +0200

    Leetcode - 647 - Setup

---
+++ LeetCode/2Medium/java/src/PalindromicSubstrings.java
@@ -0,0 +1,14 @@
+public class PalindromicSubstrings {
+    public int countSubstrings(String s) {
+        // TODO: Implement me
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        PalindromicSubstrings solution = new PalindromicSubstrings();
+        int res1 = solution.countSubstrings("abc"); //3
+        int res2 = solution.countSubstrings("aaa"); //6
+        System.out.println("Result: " + res1 + ", " + res2);
+    }
+}
