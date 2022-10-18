bd82412cdb1dc3d62898e2ffbb53c259c0e80bbb
= <sorentoennesen@gmail.com>
2022-10-19 00:01:24 +0200

    Leetcode - 22 - Implemented

--- LeetCode/2Medium/java/src/GenerateParentheses.java
+++ LeetCode/2Medium/java/src/GenerateParentheses.java
@@ -2,10 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
+    private void backtrack(List<String> res, String currentString, int open, int close, int max) {
+        if (currentString.length() == max * 2) {
+            res.add(currentString);
+            return;
+        }
+        if (open < max) backtrack(res, currentString + "(", open + 1, close, max);
+        if (close < open) backtrack(res, currentString + ")", open, close + 1, max);
+    }
     public List<String> generateParenthesis(int n) {
-        // TODO: Implement me
-
-        return new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        backtrack(res, "", 0, 0, n);
+        return res;
     }
 
     public static void main(String[] args) {
@@ -15,8 +23,8 @@ public class GenerateParentheses {
         System.out.println("Result: ");
         for (int i = 0; i < res1.size(); i++) {
             System.out.print(res1.get(i) + ", ");
-        System.out.println(" and ");
         }
+        System.out.println(" and ");
         for (int i = 0; i < res2.size(); i++) {
             System.out.print(res2.get(i) + ", ");
         }
